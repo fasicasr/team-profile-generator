69e662e68074fc89e36a231c2598774055cf08d3
Fasica Robi <fasicasr@gmail.com>
2021-04-22 00:06:11 -0400

    added jest and package.json

---
+++ test/employee.test.js
@@ -0,0 +1,21 @@
+const Employee = require("../lib/employee");
+
+describe("Employee", () => { //doesn't have exacution just used to group 
+  describe("Initialization", () => {
+    //after all the groups are created, it block takes a string to define what the test is for. Then => 
+        // it("should return an object of employee", () => {
+        //     const obj = new Employee();
+
+        //     expect("Employee" in obj).toEqual(true);
+        // });
+
+        it("should set 'name' when created", () => {
+        
+            const obj = new Employee('Jane', 1, 'jane@hotmail.com');
+    
+            expect(obj.name).toEqual('Jane');
+            expect(obj.id).toEqual(1);
+            expect(obj.email).toEqual('jane@hotmail.com');
+        });
+    });
+});
