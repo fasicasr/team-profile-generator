034739cb541e7ab53d0f68df5daecc01a995d28d
Fasica Robi <fasicasr@gmail.com>
2021-04-24 21:28:08 -0400

    fixed styling

--- index.js
+++ index.js
@@ -156,15 +156,18 @@ function writeToFile() {
            <meta http-equiv="X-UA-Compatible" content="IE=edge">
            <meta name="viewport" content="width=device-width, initial-scale=1.0">
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
            <title>Team Members</title>
        </head>
        <body>
-           <div class="jumbotron jumbotron-fluid">
+           <div class="text-white bg-dark jumbotron jumbotron-fluid">
                <div class="container">
-               <h1 class="text-center display-6">My Team</h1>
-               <p class="lead">Look before for contact information</p>
+               <h1 class="text-center display-2">My Team</h1>
+               <p class="text-center lead">Look below for contact information:)</p>
                </div>
-           </div>`
+           </div>
+           <div class = "col-md-8 offset-md-5">`
+
     let data2 = ''
     let askQuestion = ''
     for (i = 0; i < teamMember.length; i++) { 
@@ -172,7 +175,8 @@ function writeToFile() {
                 askQuestion = 
                     `<div class="card text-black bg-info mb-3" style="width: 18rem;">
                     <div class="card-header">
-                        Name:${teamMember[i].name} Manager
+                        Name:${teamMember[i].name} 
+                        <i class="fa fa-briefcase" aria-hidden="true"></i> Manager
                     </div>
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${teamMember[i].id}</li>
@@ -184,7 +188,8 @@ function writeToFile() {
                 askQuestion = 
                 `<div class="card text-black bg-info mb-3" style="width: 18rem;">
                     <div class="card-header">
-                        Name:${teamMember[i].name} Engineer
+                        Name:${teamMember[i].name} 
+                        <i class="fa fa-user-circle" aria-hidden="true"></i> Engineer
                     </div>
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${teamMember[i].id}</li>
@@ -196,7 +201,8 @@ function writeToFile() {
                 askQuestion = 
                 `<div class="card text-black bg-info mb-3" style="width: 18rem;">
                     <div class="card-header">
-                        Name:${teamMember[i].name} Intern
+                        Name:${teamMember[i].name}
+                        <i class="fa fa-graduation-cap" aria-hidden="true"></i> Intern
                     </div>
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${teamMember[i].id}</li>
@@ -209,8 +215,9 @@ function writeToFile() {
 
     }
 
-    let data3 =
-        `</body>
+    let data3 = 
+        ` </div> 
+        </body>
        </html>`;
     data = data1 + data2 + data3
     fs.writeFile(fileName, data, err =>
